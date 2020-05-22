4237015a215050295c5f014ff364f2da03fc45b4
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2020-05-21 22:08:44 -0400

    remove symlinks and consolidate install scripts
    this marks the transition into just maintaining a git repo in our home
    directory as we remove the symlink logic

--- meta/install.sh
+++ meta/install.sh
@@ -1,32 +1,227 @@
 #!/bin/sh
-# shellcheck disable=SC2012,SC2016
 
-metafiles="$(awk '/metaf/,/endmetaf/' .gitignore | awk '/!/' | cut -c2- | tr '\n' '|' | sed 's/.$//')"
-configs="$(git ls-files | cut -d/ -f1 | sort | uniq | grep -vEx "$metafiles")"
+set -eu
 
-echo "Removing existing symlinks in \$HOME pointing to this directory"
-ls -al ~ | awk -v "pwd=$PWD" '/^l/ && $11~pwd {print $9}' | xargs -I% sh -c 'echo rm -f "$HOME/%"; rm -f "$HOME/%"'
+install_bash_completions() {
+    v='2.10'
+    url="https://github.com/scop/bash-completion/releases/download/$v/bash-completion-$v.tar.xz"
 
-for c in $configs; do
-        d="$HOME/$c"
-        if [ -e "$d" ] && ! [ -L "$d" ]; then
-                >&2 echo "File \$HOME/$c already exists and isn't a symlink. Backing up to \$HOME/$c.bak."
-                mv "$d" "$d.bak"
-        fi
+    if ! [ -r ~/local/share/bash-completion/bash_completion ]; then
+        echo "Bash completion package is missing"
+        echo "Downloading"
+        get bash-completion.tz "$url"
 
-        ln -s "$PWD/$c" -t "$HOME"
-done
+        echo "Extracting"
+        rm -rf bash-completion; mkdir bash-completion
+        tar fx bash-completion.tz --strip-components 1 -C bash-completion
 
-echo "Created symlinks in \$HOME for top-level files:"
-echo "$configs" | \
-        xargs -I% ls -l ~/% | \
-        awk '{printf "- %-30s -> %s\n",$(NF-2),$NF}' | \
-        sort
+        echo "Compiling"
+        cd bash-completion
+        ./configure --prefix "$HOME/local"
+        make
+        make install
+    fi
 
-echo "Installing VIM plugins"
-nvim +PlugInstall +qa
+    completions_dir="$HOME/local/share/bash-completion/completions"
 
-echo "Templating Alacritty config"
-<.config/alacritty/alacritty.tmpl.yml envsubst '$HOME' > .config/alacritty/alacritty.yml
+    if ! [ -r "$completions_dir/git" ]; then
+        git_v="$(git --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"
+        git_url="https://raw.githubusercontent.com/git/git/v$git_v/contrib/completion/git-completion.bash"
+        get "$completions_dir/git" "$git_url"
+    fi
+}
 
-echo "Done"
+
+install_nvim() {
+    v='0.4.3'
+    url="https://github.com/neovim/neovim/releases/download/v$v/nvim-linux64.tar.gz"
+
+    if ! command -v ~/bin/nvim >/dev/null; then
+        echo "Neovim is missing"
+        echo "Downloading Neovim"
+        get nvim.tz "$url"
+        echo "Extracting tarball to ~/local"
+        tar fx nvim.tz -C ~/local
+    fi
+
+    ln -sf ~/local/nvim-linux64/bin/nvim -t ~/bin
+    nvim --version
+
+    echo "Installing VIM plugins"
+    nvim +PlugInstall +qa
+}
+
+install_dircolorshex() {
+    url='https://raw.githubusercontent.com/andreykaipov/dircolors.hex/master/bin/dircolors.hex'
+
+    if ! command -v ~/dircolors.hex; then
+        get dircolors.hex "$url"
+        chmod +x dircolors.hex
+        mv dircolors.hex ~/bin
+    fi
+
+    command -v dircolors.hex
+}
+
+install_win32yank() {
+    v='0.0.4'
+    url="https://github.com/equalsraf/win32yank/releases/download/v$v/win32yank-x64.zip"
+
+    if [ -z "$WSL_DISTRO_NAME" ]; then
+        echo "Not running within WSL distro; skipping"
+    fi
+
+    if ! command -v ~/bin/win32yank.exe >/dev/null; then
+        echo "win32yank.exe is missing"
+        echo "Downloading it"
+        get win32yank.zip "$url"
+        echo "Extracting it"
+        unzip -p win32yank.zip win32yank.exe > ~/bin/win32yank.exe
+        chmod +x ~/bin/win32yank.exe
+    fi
+
+    command -v win32yank.exe
+}
+
+
+install_shellcheck() {
+    v='0.7.1'
+    url="https://github.com/koalaman/shellcheck/releases/download/v$v/shellcheck-v$v.linux.x86_64.tar.xz"
+
+    if ! command -v ~/bin/shellcheck >/dev/null; then
+        echo "shellcheck is either missing or has a mismatched version"
+        echo "Downloading shellcheck"
+        get shellcheck.tz "$url"
+        tar fx shellcheck.tz --strip-components 1 -C ~/bin --wildcards '*/shellcheck'
+    fi
+
+    shellcheck --version
+}
+
+install_go() {
+    v='1.14.2'
+    url="https://dl.google.com/go/go$v.linux-amd64.tar.gz"
+    checksum="$(curl -sL "$url.sha256")"
+
+    if ! [ -r go.tz ]; then
+        echo "Downloading Go tarball..."
+        get go.tz "$url"
+    fi
+
+    if echo "$checksum go.tz" | sha256sum --check --status; then
+        echo "Go tarball checksums match"
+    else
+        echo "Go tarball checksums do not match"
+        echo "Verify installation script"
+        exit 2
+    fi
+
+    lastfile="$(tar ft go.tz | tail -n1)"
+    if [ -r "$HOME/local/$lastfile" ]; then
+        echo "Looks like we've already extracted the tarball"
+        echo "You might want to verify the Go installation"
+    else
+        echo "Extracting tarball to ~/local..."
+        tar fx go.tz -C ~/local/ --totals
+    fi
+
+    echo "Creating symlinks from ~/local/go/bin to ~/bin"
+    for b in ~/local/go/bin/*; do
+        ln -sf "$b" -t ~/bin
+    done
+
+    go version
+}
+
+install_dockercli() {
+    v='19.03.8'
+    url="https://download.docker.com/linux/static/stable/x86_64/docker-$v.tgz"
+
+    if ! command -v ~/bin/docker >/dev/null; then
+        echo "Downloading Docker tarball"
+        get docker.tz "$url"
+        tar fx docker.tz --strip-components 1 -C ~/bin --wildcards '*/docker'
+    fi
+
+    docker version -f '{{.Client}}' || true
+    printf "\e[1;35mNOTE: Only the CLI has been installed. For functionality, please set DOCKER_HOST in your environment.\e[0m\n"
+}
+
+install_pyenv() {
+    curl https://pyenv.run | sh
+
+}
+
+install_jq() {
+    v='1.6'
+    url="https://github.com/stedolan/jq/releases/download/jq-$v/jq-linux64"
+
+    if ! command -v ~/bin/jq >/dev/null; then
+        echo "Downloading jq"
+        get jq "$url"
+        chmod +x jq
+        mv jq ~/bin
+    fi
+
+    jq --version
+}
+
+install_upx() {
+    v='3.96'
+    url="https://github.com/upx/upx/releases/download/v$v/upx-$v-amd64_linux.tar.xz"
+
+    if ! command -v ~/bin/upx >/dev/null; then
+        echo "Downloading UPX"
+        get upx.tz "$url"
+        tar fx upx.tz --strip-components 1 -C ~/bin --wildcards '*/upx'
+    fi
+
+    # upx --version
+}
+
+install_yash() {
+    v='2.49'
+    url="https://github.com/magicant/yash/releases/download/$v/yash-$v.tar.xz"
+
+    if ! command -v ~/bin/yash >/dev/null; then
+        echo "Downloading yash"
+        get yash.tz "$url"
+        mkdir -p yash
+        tar fx yash.tz -C yash
+    fi
+}
+
+get() {
+    if command -v wget >/dev/null; then
+        wget -qO "$@"
+    elif command -v curl >/dev/null; then
+        curl -sLo "$@"
+    else
+        echo "How does this system not have neither wget nor curl?"
+        exit 1
+    fi
+}
+
+main() {
+    echo "Moving to /tmp"
+    cd /tmp || exit
+
+    echo "Creating ~/local and ~/bin directories"
+    mkdir -p ~/local
+    mkdir -p ~/bin
+
+    echo
+    for o in bash_completions nvim dircolorshex win32yank shellcheck jq upx go dockercli; do
+        echo "Installing $o"
+        install_$o
+        echo
+    done
+
+    cd -
+    echo "Templating Alacritty config"
+    <.config/alacritty/alacritty.tmpl.yml envsubst "\$HOME" > .config/alacritty/alacritty.yml
+
+    echo "Done"
+}
+
+main
