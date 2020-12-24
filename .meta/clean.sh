e693369e4ad5e9407895f1cdd9325cc5f2cb593c
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2020-12-24 01:21:00 -0500

    narrow down clean

--- .meta/clean.sh
+++ .meta/clean.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-todo="$(git clean -xdffn)"
+cmd='
+    git clean -xdff
+    -e .config/sh/env.work
+    -e .config/nix/links
+    -e .config/nvim/autoload
+    .config bin/
+'
+
+todo="$($cmd -n)"
 
 if [ -z "$todo" ]; then
         echo "Configuration is already pristine!"
@@ -9,5 +17,5 @@ else
         echo
         echo "press <enter> to continue, <ctrl>+c (^C) to cancel"
         read -r
-        git clean -xdff
+        $cmd
 fi
