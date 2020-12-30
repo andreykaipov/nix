79b794556f863cc05f4d9f446dea1fafb9430538
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2020-12-30 14:43:46 -0500

    cleanup un/install scripts for user Nix

--- .meta/install.sh
+++ .meta/install.sh
@@ -1,33 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-# see https://nixos.org/manual/nix/stable/#sect-multi-user-installation
+main() {
+    ensure_nix
+    nix-env -iA nixpkgs.mine
+    nvim +PlugInstall +qa
+    ensure_apps
+    echo "Done"
+}
+
+# see https://nixos.org/manual/nix/stable/#sect-single-user-installation
 # and https://nixos.org/manual/nix/stable/#sect-macos-installation
+ensure_nix() {
+    case "$(uname -s)" in
+        Linux)  os=linux ;;
+        Darwin) os=darwin ;;
+        *)      >&2 echo "Unknown OS"; exit 1;;
+    esac
 
-nixprofiles=/nix/var/nix/profiles
-usernixchannels="$nixprofiles/per-user/$USER/channels"
+    if [ "$os" = darwin ]; then
+        nixargs="--darwin-use-unencrypted-nix-store-volume";
+    fi
 
-# Install Nix
+    nixsh="$HOME/.nix-profile/etc/profile.d/nix.sh"
 
-if ! [ -L "$usernixchannels/manifest.nix" ]; then
-    curl -sL https://nixos.org/nix/install | sh -s -- --daemon --darwin-use-unencrypted-nix-store-volume
-    . "$nixprofiles/default/etc/profile.d/nix-daemon.sh"
-    nix-shell -p nix-info --run "nix-info -m"
-fi
+    if [ -n "$FORCE_REINSTALL" ]; then
+        nix-env -e '*'
+        rm -f "$nixsh"
+    fi
 
-# Install Nix modules for macOS
+    if ! command -v nix >/dev/null || ! [ -r "$nixsh" ]; then
+        export NIX_INSTALLER_NO_MODIFY_PROFILE=1
+        curl -sL https://nixos.org/nix/install | sh -s -- --no-daemon $nixargs
+        . "$nixsh"
+        nix-shell -p nix-info --run "nix-info -m"
+    fi
 
-if ! [ -L "$usernixchannels/darwin" ]; then
-    nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-    yes | ./result/bin/darwin-installer
+    echo "Nix is installed"
+}
 
-    # manual workaround for https://github.com/LnL7/nix-darwin/issues/149
-    sudo mv /etc/nix/nix.conf /etc/nix/nix.conf.backup-before-nix-darwin
+ensure_apps() {
+    echo "Ensuring apps"
 
-    . /etc/static/bashrc
-    time darwin-rebuild switch -I "darwin-config=$HOME/.config/nix/config.nix" --keep-failed
+    IFS=$'\n'
+    hashApp() {
+        path="$1/Contents/MacOS"; shift
+        for bin in $(find "$path" -perm +111 -type f -maxdepth 1 2>/dev/null); do
+            md5sum "$bin" | cut -b-32
+        done | md5sum | cut -b-32
+    }
 
-    rm -rf ~/.nixpkgs # default location of the nix-darwin config
-    rm -f result      # downloaded darwin installer
-fi
+    mkdir -p ~/Applications/Nix\ Apps
+
+    for app in $(find /nix/store/*my-packages/Applications/*.app -maxdepth 1 -type l); do
+        echo "$app"
+        name="$(basename "$app")"
+        src="$(/usr/bin/stat -f%Y "$app")"
+        dst="$HOME/Applications/Nix Apps/$name"
+        hash1="$(hashApp "$src")"
+        hash2="$(hashApp "$dst")"
+        if [ "$hash1" != "$hash2" ]; then
+            echo "Current hash of '$name' differs than the Nix store's. Overwriting..."
+            cp -R "$src" ~/Applications/Nix\ Apps
+            echo "Done"
+        fi
+    done
+}
+
+main
--- .meta/uninstall.sh
+++ .meta/uninstall.sh
@@ -4,26 +4,16 @@
 
 chsh -s /bin/bash
 
-# Uninstall Nix modules for macOS
-
-nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A uninstaller
-yes | ./result/bin/darwin-uninstaller
-
 # Uninstall Nix
 
-sudo launchctl unload /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-sudo rm /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-sudo mv /etc/bashrc.backup-before-nix /etc/bashrc
-sudo mv /etc/zshrc.backup-before-nix /etc/zshrc
-sudo rm -rf /etc/nix /nix /var/root/.nix-profile /var/root/.nix-defexpr /var/root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels ~/.nixpkgs
-
-sudo rm -rf ~/.config/nix/links      # created by ~/.config/nix/shell
-sudo rm -rf ~/Applications/Nix\ Apps # created by ~/.config/nix/packages
+rm -rf ~/.nix-profile ~/.nix-defexpr ~/.nix-channels
+rm -rf /Applications/Nix\ Apps # created by the configs in ~/.nixpkgs/apps
 
 # Delete the APFS volume created by the macOS installation for the Nix store at /nix
 
-diskutil unmountDisk force /nix
-diskutil apfs deleteVolume 'Nix Store'
+# sudo rm -rf /nix
+# diskutil unmountDisk force /nix
+# diskutil apfs deleteVolume 'Nix Store'
 
 if grep -q LABEL=Nix /etc/fstab; then
     echo "Hey! Remove the Nix Store entry from /etc/fstab using 'sudo vifs'"
@@ -32,5 +22,3 @@ fi
 if grep -q ^nix$ /etc/synthetic.conf; then
     echo "Hey! Remove the nix entry from /etc/synthetic.conf"
 fi
-
-sudo rm -rf /nix
