da6d8dcdfa8d0ca166573fd48a22c089775547bf
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-03-07 22:02:03 -0500

    use latest and stable channels in nixpkgs config

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,31 +1,44 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  latest ? import <unstable> {},
+  stable ? import <stable> {},
+  ...
+}:
 
 let
-  common = with pkgs; [
-    (callPackage ./cli/dircolors.hex {})
-    (callPackage ./cli/neovim {})
+  common = flatten [
+    (with stable; [
+      (callPackage ./cli/dircolors.hex {})
+      (callPackage ./cli/neovim {})
+      asciinema
+      bash-completion
+      bashInteractive_5
+      bat
+      cmake
+      ffmpeg
+      fzf
+      gifsicle
+      go
+      go-2fa
+      htop
+      imagemagick
+      jq
+      shellcheck
+      terraform-docs
+      tflint
+      tmux
+      tre-command
+      tree
+      upx
+      yq-go
+    ])
 
-    tmux
-    (callPackage ./cli/tmux-mem-cpu-load {})
-
-    bat
-    bashInteractive_5
-    bash-completion
-    go
-    go-2fa
-    htop
-    jq
-    shellcheck
-    terraform_0_14
-    terragrunt
-    tflint
-    tre-command
-    tree
-    upx
-    yarn
+    (with latest; [
+      terraform_0_14
+      terragrunt
+    ])
   ];
 
-  forDarwin  = with pkgs; [
+  forDarwin = with stable; [
     (callPackage ./apps/1password {})
     (callPackage ./apps/barrier {})
     (callPackage ./apps/discord {})
@@ -33,37 +46,37 @@ let
     (callPackage ./apps/iterm2 {})
     (callPackage ./apps/rectangle {})
     (callPackage ./apps/spotify {})
-
     coreutils
-    gifsicle
-    nodePackages.http-server
-    nodejs-14_x
-
-    vault
-    (callPackage ./cli/safe-v0.9.9 {})
-
-    # todo maybe make this a platform-agnostic script
-    (callPackage ./cli/tmux-spotify-info {})
   ];
 
-  forLinux = with pkgs; [];
+  forLinux = with stable; [];
 
-  inherit (pkgs.lib) optional flatten;
-  inherit (pkgs.stdenv) isDarwin isLinux;
+  forWork = with stable; [
+    (callPackage ./cli/fly-v4.2.5 {})
+    (callPackage ./cli/safe-v0.9.9 {})
+    (callPackage ./cli/spruce-v1.18.2 {})
+    google-cloud-sdk
+    kubectl
+    kubernetes-helm
+    nodePackages.http-server
+    nodejs-14_x
+    vault
+    yarn
+  ];
 
-  platform =
-    if isDarwin then "macosx"
-    else if isLinux then "linux"
-    else throw "unsupported platform";
+  inherit (stable.lib) optional flatten;
+  inherit (stable.stdenv) isDarwin isLinux;
+  isWork = builtins.pathExists ~/.config/sh/env.work;
 in
 {
-  packageOverrides = pkgs: with pkgs; {
+  packageOverrides = pkgs: with stable; {
     mine = buildEnv {
       name = "my-packages";
       paths = flatten [
         common
         (optional isDarwin forDarwin)
         (optional isLinux forLinux)
+        (optional isWork forWork)
       ];
     };
   };
