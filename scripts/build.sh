bface7385ec921496ed2aba37f43d264092cd102
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-05-02 23:05:55 -0400

    scripts: break up build/dev

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+if [ -z "${CI-}" ] && [ -z "${IN_NIX_SHELL-}" ]; then
+	nix-shell --run "$0"
+	exit
+fi
+
+printf "\e[1;33m%s\e[0m\n" "Running in nix-shell or in CI"
+
+mkdir -p out
+git submodule update --init moderncv
+cd moderncv
+ln -sf ../resume.tex ../custom -t .
+tectonic resume.tex -o ../out
