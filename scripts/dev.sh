a6c9af5b36e7e67fbb1d754fa5d87439d8bfe772
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-05-03 01:32:28 -0400

    scripts: comments

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Rebuilds our resume whenever any matching .tex file changes. If paired with
+# a PDF viewer that automatically reloads modified documents, we've got one
+# spicy IDE cooking!
+
 set -eu
 
 if [ -z "${IN_NIX_SHELL-}" ]; then
