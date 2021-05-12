8b28a072dab747f6cdd36d854237f06639c050fd
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-05-12 02:01:51 -0400

    infra: need to use Cloudflare pages if I want to route paths with workers

--- infra/main.tf
+++ infra/main.tf
@@ -54,16 +54,17 @@ resource "cloudflare_zone_settings_override" "kaipov" {
 
 # Gonna try out Vercel instead of Cloudflare pages
 
+/*
 resource "cloudflare_record" "pages" {
   zone_id = cloudflare_zone.kaipov.id
   name    = cloudflare_zone.kaipov.zone
   type    = "CNAME"
   value   = "cname.vercel-dns.com"
-  proxied = true
+  proxied = false # Vercel doesn't want us to proxy
   ttl     = 1
 }
+*/
 
-/*
 resource "cloudflare_record" "pages" {
   zone_id = cloudflare_zone.kaipov.id
   name    = cloudflare_zone.kaipov.zone
@@ -72,4 +73,3 @@ resource "cloudflare_record" "pages" {
   proxied = true
   ttl     = 1
 }
-*/
