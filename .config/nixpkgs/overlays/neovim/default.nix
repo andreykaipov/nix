ee37498597a139833a943f10cd899c4490b88de9
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-06-09 18:40:35 -0400

    nixpkgs: remove old redundant cli/neovim since we have an overlay

--- .config/nixpkgs/overlays/neovim/default.nix
+++ .config/nixpkgs/overlays/neovim/default.nix
@@ -1,3 +1,12 @@
+# There's no section on overlays, but it's sorta similar
+#
+# https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/vim.section.md
+# https://nixos.wiki/wiki/Vim#Custom_setup_without_using_Home_Manager
+#
+# Plugins refer to the repo name only, i.e. LnL7/vim-nix is just vim-nix. Fully
+# qualified repos with author names can be found at:
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/vim-plugins/generated.nix
+
 self: super: {
   neovim = super.neovim.override(old: rec {
     viAlias = true;
