42aca07ec8aaffa70e1d95aa584a1762d68ba3a4
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-11-01 19:10:51 -0400

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,18 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 let
   name = "resume";
 in
-  stdenv.mkDerivation {
-    name = "${name}-environment";
-    buildInputs = [
-      tectonic
-      entr
-    ];
-    shellHook = ''
+stdenv.mkDerivation {
+  name = "${name}-environment";
+  buildInputs = [
+    entr
+    hugo
+    jq
+    tectonic
+    terraform
+    terragrunt
+  ];
+  shellHook = ''
     '';
-  }
+}
