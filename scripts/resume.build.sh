7a0c87912437c128256c5345429dad8c56ac4fa6
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-11-17 01:42:14 -0500

    website: rename Hugo site dir to website

--- scripts/resume.build.sh
+++ scripts/resume.build.sh
@@ -5,15 +5,15 @@
 # latest moderncv. Even though I probably don't need the latest... why not?
 #
 # The output resume.pdf pops out at the root of this repo and is then moved to
-# site/static.
+# website/static.
 
 set -eu
 root="$(git rev-parse --show-toplevel)"
 cd "$root"
 
 if ! command -v tectonic >/dev/null; then
-	printf "\e[1;35m%s\e[0m\n" "Will invoke Tectonic via Nix..."
-	tectonic() { nix run nixpkgs.tectonic -c tectonic "$@"; }
+        printf "\e[1;35m%s\e[0m\n" "Will invoke Tectonic via Nix..."
+        tectonic() { nix run nixpkgs.tectonic -c tectonic "$@"; }
 fi
 
 printf "\e[1;36m%s\e[0m\n" "Building our resume..."
@@ -25,5 +25,5 @@ tectonic resume.tex -o "$root"
 git clean -f
 cd -
 
-printf "\e[1;36m%s\e[0m\n" "Moving resume.pdf to site/static"
-mv resume.pdf site/static
+printf "\e[1;36m%s\e[0m\n" "Moving resume.pdf to website/static"
+mv resume.pdf website/static
