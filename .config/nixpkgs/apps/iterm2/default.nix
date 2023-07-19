b1fc6f9141a36ee055de3088f1adcce239537c62
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-07-19 00:14:08 -0400

    remove .config/nixpkgs (mac apps too)

--- .config/nixpkgs/apps/iterm2/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-
-pkgs.iterm2.overrideAttrs (old: rec {
-  installPhase = (old.installPhase or "") + ''
-    # keeping this around so I know how to add extra steps to an install
-    # cp -R "$out/Applications/iTerm.app" "/Applications/Nix Apps/"
-  '';
-})
