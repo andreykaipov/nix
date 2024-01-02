633cd71ee7261c1269e3d92a20460bbc2239e432
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-01-01 20:16:17 -0500

    script: get secret json outside of tf
    lets me cache the secrets to work around 1password rate limits for
    service accounts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# shellcheck disable=SC2155
 
 log() { printf '\033[1;33m%s\033[0m\n' "$*" >&2; }
+
 get() {
-        val=$(op read "op://github/$1")
+        path=$1
+        val=$(op read "op://$path")
         cached=~/.cache/op/$1
         mkdir -p "$(dirname "$cached")"
         if ! [ -r "$cached" ] || find "$cached" -mtime +1 2>/dev/null | grep .; then
@@ -14,13 +15,33 @@ get() {
         cat "$cached"
 }
 
+# cache secrets because i ran into rate limits using the op cli with a service
+# account. might consider a 1password connect server but this works for now. :)
+get_secret_json() {
+        vault=$1
+        entry=$2
+        cached=~/.cache/op/$vault/$entry.json
+        mkdir -p "$(dirname "$cached")"
+        if [ -n "${NOCACHE-}" ] || ! [ -r "$cached" ] || find "$cached" -mtime +1 2>/dev/null | grep .; then
+                log "Updating secret: $cached"
+                rm -rf "$cached"
+                op --vault "$vault" item get "$entry" --format json | jq '
+                        [
+                        .fields[]
+                        | select(.section.label != null)
+                        | {(.section.label): {(.label): (.value as $raw | try ($raw|fromjson) catch $raw)}}
+                        ] | reduce .[] as $x ({}; .  * $x)
+                ' >"$cached"
+        fi
+        cat "$cached"
+}
+
 main() {
         root=$(git rev-parse --show-toplevel)
         repo=$(basename "$root")
         log "Setting up $repo"
 
         : "${OP_SERVICE_ACCOUNT_TOKEN?needs to be set for op CLI}"
-        op user get --me >&2
 
         cmd=$1
         case "$cmd" in
@@ -33,10 +54,8 @@ infra() {
         dir=$1
         shift
         export TERRAGRUNT_DEBUG=1
-        export TERRAGRUNT_WORKING_DIR="$dir"
-        export TF_BACKEND_USERNAME="$(get self/setup/tf_backend_username)"
-        export TF_BACKEND_PASSWORD="$(get self/setup/tf_backend_password)"
-        terragrunt "$@"
+        export TERRAGRUNT_WORKING_DIR="$PWD/$dir"
+        self_secrets="$(get_secret_json github self)" terragrunt "$@"
 }
 
 set -eu
