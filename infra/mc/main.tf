b3fa4c736797c9ba68a647847483f4a7e19308ce
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-01-04 18:21:54 -0500

    infra/mc: add more servers, refactor some things

--- infra/mc/main.tf
+++ infra/mc/main.tf
@@ -1,25 +1,41 @@
-module "volianski" {
-  source               = "./mcbe-on-azure"
-  name                 = "mc.volianski.com"
-  server_name          = "Magic DVD Ripper 4.3.1kg"
-  world_name           = "island-mp"
-  onepassword_vault    = "github"
-  az_service_principal = local.secrets.setup.az_service_principal
-  cf_api_token         = local.secrets.setup.cloudflare_api_token
-  bedrock_bridge_token = local.secrets.minecraft.bedrock_bridge_token_allan_server
-}
-
 locals {
-  servers = {
+  servers = [
+    {
+      name        = "island.mc.volianski.com"
+      server_name = "Magic DVD Ripper 4.3.1kg"
+      level_name  = "island-mp"
+    },
+    {
+      name        = "winrar.mc.volianski.com"
+      server_name = "WinRAR 3.60 beta 5 rucrk"
+      level_name  = "world"
+    },
+    {
+      name        = "shadow.mc.volianski.com"
+      server_name = "Passolo v5.0.007RetailCrk"
+      level_name  = "world2"
+    }
+  ]
+
+  servers_yml = yamlencode({
     check_interval         = "3m"
     deallocation_threshold = 5
-    servers = [
-      {
-        host    = "mc.volianski.com"
-        timeout = "10s"
-      },
-    ]
-  }
+    check_timeout          = "10s"
+    servers = values({
+      for server in local.servers :
+      server.name => { host = server.name }
+    })
+  })
+}
+
+module "mcserver" {
+  for_each             = { for _, v in local.servers : v.name => v }
+  source               = "./mcbe-on-azure"
+  name                 = each.value.name
+  server_name          = each.value.server_name
+  level_name           = each.value.level_name
+  onepassword_vault    = "github"
+  bedrock_bridge_token = local.secrets.minecraft.bedrock_bridge_token_allan_server
 }
 
 module "mcmanager" {
@@ -28,29 +44,19 @@ module "mcmanager" {
   name     = "mcmanager"
   location = "eastus"
   image    = "ghcr.io/andreykaipov/discord-bots/go/mcmanager"
-  sha      = "sha256:d5e86bd4c35043789aa852175d46a5a0819cebff1c0dd22e9462f3a63d650356"
-
-  secrets = {
-    discord_token      = local.secrets.minecraft.discord_token
-    az_client_id       = local.secrets.setup.az_service_principal.appId
-    az_client_secret   = local.secrets.setup.az_service_principal.password
-    az_subscription_id = local.secrets.setup.az_service_principal.subscriptionId
-    az_tenant_id       = local.secrets.setup.az_service_principal.tenantId
-  }
+  sha      = "sha256:101c588c6bd815ba4055a4fde76411e2be9f2578bd8db4fc09bdea5ff1147cb4"
 
   env = {
-    AZURE_CLIENT_ID       = "secret://az_client_id"
-    AZURE_CLIENT_SECRET   = "secret://az_client_secret"
-    AZURE_TENANT_ID       = "secret://az_tenant_id"
-    AZURE_SUBSCRIPTION_ID = "secret://az_subscription_id"
-    DISCORD_TOKEN         = "secret://discord_token",
+    AZURE_CLIENT_ID       = "secret://${local.secrets.setup.az_service_principal.appId}"
+    AZURE_CLIENT_SECRET   = "secret://${local.secrets.setup.az_service_principal.password}"
+    AZURE_TENANT_ID       = "secret://${local.secrets.setup.az_service_principal.tenantId}"
+    AZURE_SUBSCRIPTION_ID = "secret://${local.secrets.setup.az_service_principal.subscriptionId}"
+    DISCORD_TOKEN         = "secret://${local.secrets.minecraft.discord_token}"
     MGMT_CHANNEL          = "1192498155200192562",
     SERVERS_FILE          = "/shared/servers.yml",
   }
 
   files = {
-    "servers.yml" = yamlencode(local.servers)
+    "servers.yml" = local.servers_yml
   }
-
-  az_service_principal = local.secrets.setup.az_service_principal
 }
--- infra/mc/terragrunt.hcl
+++ infra/mc/terragrunt.hcl
@@ -1,6 +1,3 @@
 include "root" {
   path = find_in_parent_folders()
 }
-
-locals {
-}
