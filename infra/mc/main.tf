4a7ed23449d3901691424fe03acc032722090c8e
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-01-03 21:12:51 -0500

    infra: add mc.volianski.com

---
+++ infra/mc/main.tf
@@ -0,0 +1,9 @@
+module "volianski" {
+  source               = "./mcbe-on-azure"
+  name                 = "mc.volianski.com"
+  server_name          = "Magic DVD Ripper 4.3.1kg"
+  world_name           = "island-mp"
+  onepassword_vault    = "github"
+  az_service_principal = local.secrets.setup.az_service_principal
+  cf_api_token         = local.secrets.setup.cloudflare_api_token
+}
