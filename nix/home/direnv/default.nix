2ff5b3abe3d4bf1c62c6088272d20ec6a97d897b
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-03-03 18:44:53 -0500

    nix: final touches

--- nix/home/direnv/default.nix
+++ nix/home/direnv/default.nix
@@ -4,4 +4,9 @@
 {
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
+
+  # quiets direnv (instead look for the duck emoji in our prompt)
+  programs.zsh.envExtra = ''
+    export DIRENV_LOG_FORMAT=
+  '';
 }
