286e668ee352fde65d5f8c2884350a7851e48aba
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-03-03 18:11:04 -0500

    nix: add zellij hm import

--- nix/home/zellij/default.nix
+++ nix/home/zellij/default.nix
@@ -3,5 +3,9 @@
 }:
 {
   programs.zellij.enable = true;
-  programs.zellij.enableZshIntegration = true;
+
+  # see scripts/_bootstrap.sh instead
+  programs.zellij.enableZshIntegration = false;
+
+  home.file.".config/zellij/config.kdl".source = ./config.kdl;
 }
