7cf8a60c864d9670da7f9bd5e8036364bf9bf776
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-05 19:16:04 -0500

    nix.conf settings

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,17 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    # nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     devenv.url = "github:cachix/devenv/latest"; # don't follow
 
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    neovim-nightly-overlay.inputs.neovim-flake.url = "github:neovim/neovim?dir=contrib"; #&rev=eb151a9730f0000ff46e0b3467e29bb9f02ae362";
-    neovim-nightly-overlay.inputs.neovim-flake.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    neovim-nightly.url = "github:neovim/neovim?dir=contrib"; #" #&rev=eb151a9730f0000ff46e0b3467e29bb9f02ae362";
+    neovim-nightly.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # zsh plugins
     zsh-powerlevel10k.url = "github:romkatv/powerlevel10k";
@@ -31,40 +29,43 @@
     inputs @ { self
     , nixpkgs
     , nixpkgs-unstable
-    , nixpkgs-master
+      # , nixpkgs-master
     , home-manager
+    , neovim-nightly
     , devenv
-    , neovim-nightly-overlay
     , ...
     }:
     let
-      lib = nixpkgs-unstable.lib;
+      # lib = nixpkgs-unstable.lib;
       homeConfig = system: hostname:
         let
-          cfg = import ./hosts/${hostname}.nix;
+          pkgs = nixpkgs-unstable.legacyPackages.${system};
+          lib = nixpkgs.lib.extend (libself: super: {
+            my = import ./lib {
+              inherit system pkgs;
+              lib = libself;
+              flake = self;
+            };
+          });
+          cfg = import ./hosts/${hostname}.nix { inherit lib; };
           username = cfg.username;
           homedir = lib.attrsets.attrByPath [ "homedir" ] "" cfg;
           extraModules = cfg.extraModules;
         in
         home-manager.lib.homeManagerConfiguration
           rec {
-            pkgs = nixpkgs-unstable.legacyPackages.${system};
-            # pkgs = import <nixpkgs> { }; # alternative to above line, but this is impure
+            inherit lib pkgs;
 
-            lib = nixpkgs.lib.extend (libself: super: {
-              my = import ./lib {
-                inherit system pkgs;
-                lib = libself;
-                flake = self;
-              };
-            });
+            # pkgs = import <nixpkgs> { }; # alternative to above line, but this is impure
 
             modules = [
               {
                 # alternatively, we can set these in `import nixpkgs { ... }` instead of using legacyPackages above
                 nixpkgs.config.allowUnfreePredicate = (pkg: true); # https://github.com/nix-community/home-manager/issues/2942
                 nixpkgs.overlays = lib.my.overlays ++ [
-                  # neovim-nightly-overlay.overlay
+                  (final: prev: {
+                    neovim-nightly = neovim-nightly.packages.${prev.system}.neovim;
+                  })
                 ];
               }
               {
@@ -87,7 +88,8 @@
     {
       homeConfigurations = builtins.mapAttrs (hostname: configurer: configurer hostname) {
         dustbox = homeConfig "x86_64-linux";
-        smart-toaster = homeConfig "x86_64-darwin"; # this is an m1 but aarch64-darwin doesn't work?
+        # smart-toaster = homeConfig "x86_64-darwin"; # this is an m1 but aarch64-darwin doesn't work?
+        smart-toaster = homeConfig "aarch64-darwin";
       };
     };
 }
