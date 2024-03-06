5bd646ccfc04cdb838f9916a89adc9486acf73b6
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-05 19:13:12 -0500

    zsh whatever

--- nix/home/zsh/default.nix
+++ nix/home/zsh/default.nix
@@ -17,19 +17,38 @@
     autocd = true;
     defaultKeymap = "emacs";
     dotDir = ".config/zsh";
-    history = rec {
-      path = "${config.xdg.dataHome}/zsh/history"; # ~/.local/share/...
-      share = true;
-      extended = true;
-      save = 100000;
-      size = save;
-      ignorePatterns = [ "rm *" ];
-      ignoreDups = true;
-      ignoreAllDups = false;
-      ignoreSpace = false;
-      expireDuplicatesFirst = false;
-    };
     syntaxHighlighting.enable = true;
+    envExtra = ''
+      export FZF_BASE=${pkgs.fzf}/share/fzf
+      ${builtins.readFile ./zshenv}
+    '';
+    profileExtra = ''
+      . ${pkgs.nix.outPath}/etc/profile.d/nix.sh
+      export PATH=~/bin:$PATH
+    '';
+    initExtraFirst = ''
+      # To customize prompt, run `p10k configure` or edit p10k.zsh.
+      # The following enables instant-prompt for p10k. It should be at the top of our generated ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n] confirmations, etc.)
+      # can go here too, but should be above this conditional.
+      # ref: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#instant-prompt
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
+    initExtraBeforeCompInit = '' # init extra before compinit '';
+    oh-my-zsh.extraConfig = ''
+      # happens before oh-my-zsh is sourced
+
+      # specifically bracketed paste
+      # without this pasting into the terminal is super slow
+      export DISABLE_MAGIC_FUNCTIONS=true
+
+      # appends to fpath
+      # sourcing these completions has to be before oh-my-zsh is sourced
+      # ref: https://github.com/zsh-users/zsh-completions/issues/603
+      source "${inputs.zsh-completions}/zsh-completions.plugin.zsh"
+    '';
     oh-my-zsh.enable = true;
     oh-my-zsh.plugins = [
       # https://github.com/ohmyzsh/ohmyzsh/wiki/Plugins
@@ -43,32 +62,22 @@
       "terraform"
       "themes"
     ];
-    oh-my-zsh.extraConfig = ''
-      # happens before oh-my-zsh is sourced
-      # appends to fpath
-      # sourcing these completions has to be before oh-my-zsh is sourced
-      # ref: https://github.com/zsh-users/zsh-completions/issues/603
-      source "${inputs.zsh-completions}/zsh-completions.plugin.zsh"
-    '';
-    envExtra = ''
-      . ${pkgs.nix.outPath}/etc/profile.d/nix.sh
-      export FZF_BASE=${pkgs.fzf}/share/fzf
-      ${builtins.readFile ./zshenv}
-      export PATH=~/bin:$PATH
-    '';
-    initExtraFirst = ''
-      # To customize prompt, run `p10k configure` or edit p10k.zsh.
-      # The following enables instant-prompt for p10k. It should be at the top of our generated ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n] confirmations, etc.)
-      # can go here too, but should be above this conditional.
-      # ref: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#instant-prompt
-      # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-      #   source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      # fi
-    '';
-    initExtraBeforeCompInit = ''
-    '';
+    history = rec {
+      path = "${config.xdg.dataHome}/zsh/history"; # ~/.local/share/...
+      share = false;
+      extended = true;
+      save = 100000;
+      size = save;
+      ignorePatterns = [ "rm *" ];
+      ignoreDups = true; # makes searching history faster
+      ignoreAllDups = true;
+      ignoreSpace = false;
+      expireDuplicatesFirst = false;
+    };
     initExtra = ''
+      # so new shells will get the history of all previous shells
+      setopt INC_APPEND_HISTORY_TIME
+
       # autoload -U +X bashcompinit && bashcompinit
       complete -o nospace -C ${pkgs.terragrunt}/bin/terragrunt terragrunt
 
@@ -78,6 +87,7 @@
       source "${inputs.zsh-powerlevel10k}/powerlevel10k.zsh-theme"
       source "${inputs.zsh-fzf-tab}/fzf-tab.plugin.zsh"
       source "${inputs.zsh-fzf-tab-source}/fzf-tab-source.plugin.zsh"
+
       ### our zshrc
       ${builtins.readFile ./zshrc}
     '';
