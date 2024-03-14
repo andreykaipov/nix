e2c3a7e2b178e6020ea29b8a4f9b67a92d2681ba
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-14 00:34:57 -0400

    nix: split out home imports

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -1,128 +1,14 @@
-{ config
-, lib
-, pkgs
-, pkgs-stable
-, devenv
-, homeConfig
-, ...
-}:
-let
-  user = homeConfig.username; # could just use $USER or whoami but why not pass it
-in
+{ ... }:
 {
-  home.packages = with pkgs; [
-    _1password
-    bashInteractive
-    bash-completion
-    # dev tools
-    rclone
-    socat
-    unixtools.netstat
-    bat
-    autojump
-    cachix
-    dircolors_hex
-    fzf
-    gh
-    git
-    graphviz
-    jq
-    yq-go
-    mutagen
-    devenv
-    nmap
-    ripgrep
-    fd
-    eza
-    lesspipe
-    opentofu
-    pkgs-stable.terraform
-
-    # langs
-    cargo
-    go
-    nodejs
-    python311
-    python311Packages.pip
-    python311Packages.virtualenv
-    lua
-
-    # lsps
-    golangci-lint-langserver
-    gopls
-    lua-language-server
-    marksman
-    nil
-    nixd
-    nodePackages.bash-language-server
-    nodePackages.typescript-language-server
-    nodePackages.vim-language-server
-    terraform-ls
-    terraform-lsp # unofficial one
-    vscode-langservers-extracted
-    # yaml-language-server
-
-    # lang-specific dev/diagnostic/lint tools
-    deadnix
-    git-filter-repo
-    lazygit
-    nixpkgs-fmt
-    rufo
-    shellcheck
-    shfmt
-    statix
-    stylua
-    nodePackages.textlint
-    nodePackages.textlint-rule-common-misspellings
-    nodePackages.textlint-rule-no-start-duplicated-conjunction
-    nodePackages.textlint-rule-stop-words
-    nodePackages.textlint-rule-terminology
-    nodePackages.textlint-rule-write-good
-    yaml-language-server
-    # yamlfix
-    # yamlfmt
-    # yamllint
-
-
-  ];
-
   programs.home-manager.enable = true;
-
   imports = [
     ./bootstrap
-    ./ssh
-    ./nvim
-    ./direnv
+    ./packages
     ./zsh
     ./tmux
+    ./nvim
+    ./ssh
+    ./scripts
+    ./direnv
   ];
-
-  home.file."bin/example".text = ''but not executable'';
-
-  home.activation = lib.my.activationScripts (map toString [
-    ''
-      mkdir -p ~/.{cache,config,local,run}
-    ''
-    ''
-      echo hello??
-    ''
-    # ''
-    #   if ! grep -q discord /etc/group; then
-    #     echo "Discord group does not exist. Creating it..."
-    #     sudo groupadd discord
-    #   fi
-    #   if ! groups ${user} | grep -qw discord; then
-    #     echo "Adding ${user} to discord group..."
-    #     sudo usermod -a -G discord ${user}
-    #   fi
-    # ''
-    #./scripts/ssh-generate-authorized-keys
-    #./scripts/nvim-ensure-plugins
-    #./scripts/tmux-ensure-plugins
-  ]);
 }
-
-#I'm having similar issues trying to replicate similar behaviour to copilot.vim where I open a new line, and immediately get suggestions. I've tried editing copilot.lua settings ;
-# (pkgs.writeText "hello" ''
-#   echo ${pkgs.bash-completion}
-# '')
