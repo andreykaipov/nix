13cc42d5cdaecc6b1dd1a7c05c2b9299a0ef0725
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-14 00:34:57 -0400

    nix: split out home imports

---
+++ nix/home/packages/default.nix
@@ -0,0 +1,92 @@
+{ pkgs
+, pkgs-stable
+, devenv
+, ...
+}:
+{
+  home.packages = with pkgs; [
+    # misc non-lang specific dev tools
+    _1password
+    bat
+    cachix
+    devenv
+    dircolors_hex
+    eza
+    fd
+    fzf
+    lazygit
+    gh
+    git-filter-repo
+    git
+    graphviz
+    jq
+    lesspipe
+    mutagen
+    nmap
+    rclone
+    ripgrep
+    socat
+    yq-go
+
+    # sh
+    nodePackages.bash-language-server
+    shellcheck
+    shfmt
+
+    # go
+    go
+    golangci-lint-langserver
+    gopls
+
+    # tf
+    terraform
+    opentofu
+    terraform-ls
+
+    # nix
+    nil
+    nixd
+    deadnix
+    statix
+    nixpkgs-fmt
+
+    # lua
+    lua
+    lua-language-server
+    stylua
+
+    # yaml
+    yaml-language-server
+    yamllint
+    # pkgs-stable.yamlfix # https://github.com/NixOS/nixpkgs/issues/294005
+    # yamlfmt # probably don't need these since lsp does enough
+
+    # rust
+    cargo
+
+    # ruby
+    rufo
+
+    # python
+    python311
+    python311Packages.pip
+    python311Packages.virtualenv
+
+    # web crap
+    nodejs
+    nodePackages.typescript-language-server
+    vscode-langservers-extracted
+
+    # text
+    marksman
+    nodePackages.textlint
+    nodePackages.textlint-rule-common-misspellings
+    nodePackages.textlint-rule-no-start-duplicated-conjunction
+    nodePackages.textlint-rule-stop-words
+    nodePackages.textlint-rule-terminology
+    nodePackages.textlint-rule-write-good
+
+    # vim
+    nodePackages.vim-language-server
+  ];
+}
---
+++ nix/home/scripts/default.nix
@@ -0,0 +1,39 @@
+{ pkgs
+, lib
+, homeConfig
+, ...
+}:
+let
+  user = homeConfig.username; # could just use $USER or whoami but why not pass it
+in
+{
+  home = {
+    emptyActivationPath = false;
+
+    # https://ryantm.github.io/nixpkgs/builders/trivial-builders/#trivial-builder-writeText
+    file."bin/example".text = ''but not executable'';
+
+    activation = lib.my.activationScripts (map toString [
+      ''
+        mkdir -p ~/.{cache,config,local,run}
+      ''
+      ''
+        # echo "My path is: $PATH"
+        echo hello??
+      ''
+      # ''
+      #   if ! grep -q discord /etc/group; then
+      #     echo "Discord group does not exist. Creating it..."
+      #     sudo groupadd discord
+      #   fi
+      #   if ! groups ${user} | grep -qw discord; then
+      #     echo "Adding ${user} to discord group..."
+      #     sudo usermod -a -G discord ${user}
+      #   fi
+      # ''
+      #./scripts/ssh-generate-authorized-keys
+      #./scripts/nvim-ensure-plugins
+      #./scripts/tmux-ensure-plugins
+    ]);
+  };
+}
