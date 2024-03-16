f2da5ff9e4e83d8d20c24f0751da4385d396f995
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-16 19:03:34 -0400

    nix: cleaner host attr set

--- nix/hosts/config.nix
+++ nix/hosts/config.nix
@@ -1,20 +1,21 @@
-{ lib
+# forced pure eval with flakes will catch the import if it ever happens. it's
+# for anything outside of the flake that imports hosts.
+{ lib ? (import <nixpkgs> { }).lib
 , ...
 }:
-with lib;
 with builtins;
 let
   # read the current dir and get a list of files, excluding this one
   # ref: https://github.com/NixOS/nix/issues/5897
   currentFile = baseNameOf __curPos.file;
-  files = mapAttrsToList (name: _: name) (readDir ./.);
+  files = attrNames (readDir ./.);
   hostFiles = filter (f: f != currentFile) files;
 
   # find the hostname from the filename and make an intermmediary list to be
   # able to map each hostname to its config
   mkHost = f:
     let
-      hostname = removeSuffix ".nix" f;
+      hostname = lib.removeSuffix ".nix" f;
     in
     {
       name = hostname;
