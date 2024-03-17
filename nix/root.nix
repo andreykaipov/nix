890507695543e8cad7a2a4c97e70d5eef1c9df04
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-03-16 21:50:16 -0400

    nix: add agenix; just ssh secrets for now

--- nix/root.nix
+++ nix/root.nix
@@ -1,17 +1,14 @@
-# module that exposes its own path at `andrey.root`
-# other modules can import config and reference `config.andrey.root`
-# as an alternative to using relative paths
-# supposed to be kept alongside the flake.nix file, otherwise the path will need to be changed
-# i have no clue if this is good nix or not! :D
+# module that exposes its own path at `config.root` as an alternative to using
+# relative paths. supposed to be kept alongside the flake.nix file, otherwise
+# the path will need to be changed. i have no clue if this is good nix or not!
+# :D
 { lib
 , ...
 }:
 with lib;
 {
-  options.andrey = {
-    root = mkOption {
-      type = types.str;
-      default = builtins.toString ./.;
-    };
+  options.root = mkOption {
+    type = types.str;
+    default = builtins.toString ./.;
   };
 }
