9162171cca65f5fe14c74918f671420245a7c3cf
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-04-01 12:34:28 -0400

    nvim: use an out of store symlink

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,16 +1,8 @@
-{ pkgs
+{ config
+, pkgs
 , ...
 }: {
-  home.file = {
-    # by default these are recursive = false, so they will be symlinks to these files in the nix store
-    # meaning we still must do a home-manager switch to actually see changes to nvim regardless
-    # ref: https://nix-community.github.io/home-manager/options.xhtml#opt-home.file._name_.recursive
-    ".config/nvim/init.lua".source = ./init.lua;
-    ".config/nvim/lua".source = ./lua;
-    ".config/nvim/after".source = ./after;
-    ".config/nvim/ftdetect".source = ./ftdetect;
-    # ".config/nvim/ftplugin".source = ./ftplugin;
-  };
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.gitRoot}/home/nvim";
 
   programs.neovim = {
     package = pkgs.neovim-nightly;
