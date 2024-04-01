2bf6075ca9942823e6519770d8e3a60decfc4d15
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-04-01 10:19:19 -0400

    ssh: key symlinks false

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -9,14 +9,24 @@ let
 in
 {
   config = {
+    # TODO: set symlinks back to true. with them off, it's no different than
+    # just maintaing the keys manually, but it would be nice to have them on
+    # since reboots break the symlink and remove the decryption key from tmp
+    #
+    # would be neat to wrap the hm-switch in a script that reads the decryption
+    # key from 1password and then runs it when i open up wezterm, like in the
+    # bootstrap.sh.mustache script then we can set symlinks = true again
+
     age.decrypt."ghbastion.pem.age" = {
       path = "${config.home.homeDirectory}/.config/ssh/keys/ghbastion.pem";
       mode = "600";
+      symlink = false;
     };
 
     age.decrypt."${hostname}.pem.age" = {
       path = "${config.home.homeDirectory}/.config/ssh/keys/${hostname}.pem";
       mode = "600";
+      symlink = false;
     };
 
     # don't use programs.ssh.enable for the same reason we don't do it for tmux
