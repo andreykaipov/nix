87d101bfbf5f1687e257dc2d2ec1b2a66e6ef807
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-04-18 17:46:39 -0400

    use out of store symlinks for ssh and tmux confs

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,16 +1,15 @@
-{ pkgs
+{ config
+, pkgs
 , ...
 }:
 {
-  home = {
-    # we don't use programs.tmux.enable because we have a lot of custom written config
-    # and home-manager adds some defaults that i don't like
-    packages = with pkgs; [ tmux ];
+  # we don't use programs.tmux.enable because we have a lot of custom written
+  # configs and home-manager adds some defaults that i don't like.
+  #
+  # our tmux is spawned by our bootstrap import and will reference the config
+  # directly at $XDG_CONFIG_HOME/tmux.core.conf instead
 
-    # for example our configs expect to be in XDG_CONFIG_HOME/tmux
-    # tmux is spawned by our bootstrap import and references the config directly
-    file.".config/tmux".source = ./.;
-    file.".config/tmux".recursive = true;
-  };
+  home.packages = with pkgs; [ tmux ];
+
+  xdg.configFile."tmux".source = config.lib.file.mkOutOfStoreSymlink "${config.gitRoot}/home/tmux";
 }
-
