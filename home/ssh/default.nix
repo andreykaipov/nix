477735624333345f2c3d6e097baaca466deb35cf
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-04-18 17:46:39 -0400

    use out of store symlinks for ssh and tmux confs

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -30,13 +30,8 @@ in
     };
 
     # don't use programs.ssh.enable for the same reason we don't do it for tmux
-    home = {
-      packages = with pkgs; [ openssh ];
-      file = {
-        ".config/ssh".source = ./.;
-        ".config/ssh".recursive = true;
-      };
-    };
+    home.packages = with pkgs; [ openssh ];
+    xdg.configFile."ssh".source = config.lib.file.mkOutOfStoreSymlink "${config.gitRoot}/home/ssh";
 
     # SSH doesn't really support XDG config paths, so these are workarounds
     # See https://wiki.archlinux.org/index.php/XDG_Base_Directory
