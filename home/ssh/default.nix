927086f4b4b72eddb745bcccc5d8ded8e74ed452
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2024-08-01 21:59:58 -0400

    move fake xdg config ssh to just .ssh again

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -31,16 +31,16 @@ in
 
     # don't use programs.ssh.enable for the same reason we don't do it for tmux
     home.packages = with pkgs; [ openssh ];
-    xdg.configFile."ssh".source = config.lib.file.mkOutOfStoreSymlink "${config.gitRoot}/home/ssh";
+    home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "${config.gitRoot}/home/ssh";
 
     # SSH doesn't really support XDG config paths, so these are workarounds
     # See https://wiki.archlinux.org/index.php/XDG_Base_Directory
     #
     # Why not just let it go to ~/.ssh/config so I don't have to do this?
     # Because I'm a stubborn little bitch.
-    programs.zsh.shellAliases = {
-      ssh = "ssh -F ~/.config/ssh/config";
-      scp = "scp -F ~/.config/ssh/config";
-    };
+    # programs.zsh.shellAliases = {
+    # ssh = "ssh -F ~/.config/ssh/config";
+    # scp = "scp -F ~/.config/ssh/config";
+    # };
   };
 }
