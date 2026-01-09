e71f70fff208de33febede395861f4fc97e07fbe
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-09 17:42:52 -0500

    home: replace SSH module with raw config + config.d symlink

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -1,29 +1,13 @@
 {
+  config,
+  pkgs,
   host,
   ...
 }:
 
 {
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    includes = [
-      "${host.homeDirectory}/.ssh/config_external"
-    ];
-    matchBlocks = {
-      "*" = {
-        sendEnv = [
-          "LANG"
-          "LC_*"
-        ];
-        hashKnownHosts = true;
-      };
-      "github.com" = {
-        identitiesOnly = true;
-        identityFile = [
-          "${host.homeDirectory}/.ssh/id_github"
-        ];
-      };
-    };
-  };
+  home.packages = with pkgs; [ openssh ];
+
+  # Symlink the whole ssh module directory into ~/.ssh so config + config.d/* are live-editable
+  home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "${host.gitRoot}/modules/home/ssh";
 }
