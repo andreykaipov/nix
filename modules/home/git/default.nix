ed108e3bcd1096796cf235d010ac433e52de827a
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-09 17:21:10 -0500

    home: enrich git config (per-host SSH signing, plist diff, URL rewriting)

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,4 +1,6 @@
 {
+  pkgs,
+  host,
   ...
 }:
 
@@ -7,6 +9,13 @@ let
   email = "9457739+andreykaipov@users.noreply.github.com";
 in
 {
+  home.packages = with pkgs; [
+    git
+    git-filter-repo
+    gh
+    lazygit
+  ];
+
   programs.git = {
     enable = true;
     ignores = [ "*.swp" ];
@@ -16,7 +25,9 @@ in
     settings = {
       user = {
         inherit name email;
+        signingkey = "${host.homeDirectory}/.ssh/keys/${host.hostname}.pem.pub";
       };
+      gpg.format = "ssh";
       init.defaultBranch = "main";
       core = {
         editor = "nvim";
@@ -25,6 +36,13 @@ in
       commit.gpgsign = true;
       pull.rebase = true;
       rebase.autoStash = true;
+      credential.helper = "cache --timeout=10000";
+      "diff \"plist\"".textconv = "plutil -convert xml1 -o -";
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+      };
     };
   };
 }
