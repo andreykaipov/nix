20d25f5ba8ec5a58c60a56d9d1cf6411ffad71de
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-11 15:20:02 -0500

    chore: remove old main directory structure
    Remove home/, overlays/, packages/, and secrets/ directories
    that survived the merge from the pre-refactor branch.
    All needed config has been ported to the new modules/ structure.

--- home/nvim/after/ftplugin/lua.lua
+++
@@ -1,4 +0,0 @@
--- https://github.com/gotgenes/dotfiles/tree/main/private_dot_config/nvim/ftplugin
-
--- note both vim-sleuth and stylua.lua may overwrite options like expandtab or shiftwidth
-vim.opt.tabstop = 4
--- packages/README.md
+++
@@ -1,3 +0,0 @@
-
-ahjsd
-as
