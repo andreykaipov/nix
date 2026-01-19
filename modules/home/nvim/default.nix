7a0ec9eb7860ecd46360abaf1a4f051136221b6b
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-18 20:44:16 -0500

    refactor: move nvim-related packages to nvim module
    Move nodejs (copilot.lua server) and tree-sitter to nvim/default.nix
    since they are Neovim dependencies.

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -6,6 +6,11 @@
 }:
 
 {
+  home.packages = with pkgs; [
+    nodejs # for copilot.lua server
+    tree-sitter
+  ];
+
   xdg.configFile."nvim" = host.symlinkTo ./.;
   programs.neovim = {
     package = inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default;
