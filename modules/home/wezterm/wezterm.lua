c84d66d5c2f6e00cd9a5f6e008c22eb919643255
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-18 21:18:13 -0500

    wezterm larger

--- modules/home/wezterm/wezterm.lua
+++ modules/home/wezterm/wezterm.lua
@@ -10,11 +10,11 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.exit_behavior = 'CloseOnCleanExit'
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 180
+config.initial_rows = 45
 
 -- or, changing the font size and color scheme.
-config.font_size = 17
+config.font_size = 16
 -- config.color_scheme = 'AdventureTime'
 --
 config.colors = {
