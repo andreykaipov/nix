f9d8e1ca760b6c8b0c4a9705ddb38426afb92fe9
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-20 16:45:13 -0500

    sudo reattach

--- modules/darwin/system/default.nix
+++ modules/darwin/system/default.nix
@@ -43,6 +43,7 @@
 
   security.pam.services.sudo_local.touchIdAuth = true;
   security.pam.services.sudo_local.watchIdAuth = true;
+  security.pam.services.sudo_local.reattach = true;
   security.sudo.extraConfig = ''
     Defaults timestamp_timeout=60
   '';
