062ea6731a1f7573d7ff57c16fa5cf9a171467cc
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-01-23 15:30:06 -0500

    Bootstrap toaster

--- hosts/toaster/default.nix
+++ hosts/toaster/default.nix
@@ -4,5 +4,5 @@
 {
   system = "arm64-darwin";
   username = "andrey";
-  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc+B+mktpyIKqx8bkAWV4mwmdZ/O3zP6wRCMCQdJEwK ";
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc+B+mktpyIKqx8bkAWV4mwmdZ/O3zP6wRCMCQdJEwK";
 }
