bc92399cc0e982e81329fd01e77bcfe674ab47e8
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-02-10 20:35:08 -0500

    hosts: bootstrap airfryer

--- hosts/airfryer/default.nix
+++ hosts/airfryer/default.nix
@@ -4,5 +4,5 @@
 {
   system = "aarch64-darwin";
   username = "andrey";
-  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAS3g5ZE1lo833a6BunbgwoUL1mVqQqS4MxGdVL5am7F ";
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAS3g5ZE1lo833a6BunbgwoUL1mVqQqS4MxGdVL5am7F";
 }
