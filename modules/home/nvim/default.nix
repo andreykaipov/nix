f32419ec39b2c54e5927a47b3253cb0ccae3ba90
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-02-18 19:21:20 -0500

    nvim: ensure git is on PATH during headless plugin install

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -23,7 +23,7 @@
 
   home.activation.nvimPlugins = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     echo "Installing nvim plugins..."
-    ${
+    PATH="${pkgs.git}/bin:$PATH" ${
       inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default
     }/bin/nvim --headless +qa 2>&1 || true
   '';
