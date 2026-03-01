21ce55a953db5b08aa120c296fcbf28de14aeb14
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-03-01 17:52:57 -0500

    home: add host.symlinkTo for xdg config symlinks
    - Add hosts/extend.nix: HM module that injects symlinkTo into host
      via _module.args (needs module context for mkOutOfStoreSymlink)
    - Usage: xdg.configFile."nvim" = host.symlinkTo ./.;
    - Update nvim and tmux modules to use host.symlinkTo
    - Remove commented-out vim config from nvim module

--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -1,17 +1,10 @@
 {
-  config,
   pkgs,
   host,
   ...
 }:
 
 {
-  # we don't use programs.tmux.enable because we have a lot of custom written
-  # configs and home-manager adds shit I don't like
-  #
-  # our tmux is spawned by our bootstrap import and will reference the config
-  # directly at $XDG_CONFIG_HOME/tmux/core.conf instead
-  xdg.configFile."tmux".source =
-    config.lib.file.mkOutOfStoreSymlink "${host.gitRoot}/modules/home/tmux";
+  xdg.configFile."tmux" = host.symlinkTo ./.;
   home.packages = with pkgs; [ tmux ];
 }
