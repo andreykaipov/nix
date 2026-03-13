b1583a2d5a5233ffdc2b1869b2448088a31f7f22
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-03-13 00:20:53 -0400

    hosts: bootstrap toaster

--- hosts/toaster/default.nix
+++ hosts/toaster/default.nix
@@ -5,7 +5,7 @@
 {
   system = "arm64-darwin";
   username = "andrey";
-  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFW9lYrlUA5gWMBEBnuMCcVpBLih8KQhizcCNsSPo9U7 ";
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFW9lYrlUA5gWMBEBnuMCcVpBLih8KQhizcCNsSPo9U7";
   desktopBackground = builtins.fetchurl {
     url = "https://512pixels.net/downloads/macos-wallpapers-6k/13-Ventura-Dark.jpg";
     sha256 = "1bpkf2ibh7a9vi1p11i4ykbcs2k197d6wfbnlvf4vflnylbi5lrb";
