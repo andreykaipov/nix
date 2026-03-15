e2a850e07ae36b76bf65f6e461a48c27705c5bed
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2026-03-14 21:11:56 -0400

    add cachix substituters for nix-community and neovim-nightly

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -7,6 +7,21 @@
   # Determinate Nix manages the daemon; don't let nix-darwin conflict with it
   nix.enable = false;
 
+  # Determinate Nix reads this for user customizations (see /etc/nix/nix.conf)
+  environment.etc."nix/nix.custom.conf".text =
+    let
+      cachix = {
+        "nix-community.cachix.org" = "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+        "nix-community-neovim-nightly-overlay.cachix.org" = "WlgMaITRm3UMnm28W1mM8hBDTdBs0chO54bens56oVo=";
+      };
+      substituters = lib.mapAttrsToList (h: _: "https://${h}") cachix;
+      publicKeys = lib.mapAttrsToList (h: k: "${h}-1:${k}") cachix;
+    in
+    ''
+      extra-substituters = ${lib.concatStringsSep " " substituters}
+      extra-trusted-public-keys = ${lib.concatStringsSep " " publicKeys}
+    '';
+
   # nix-darwin's programs.zsh controls /etc/zshrc, which runs before our user
   # zshrc (~/.config/zsh/.zshrc). By default it runs compinit and prompt init
   # there, but home-manager already does both. Disable them here so /etc/zshrc
